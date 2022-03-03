15dc6c832139105d1c805d66e5c04772887e8b7d
mathieutercan <mathieu.tercan@epitech.eu>
2022-03-03 14:26:56 +0100

    [Update] Blender.sh generate video

--- Back/src/scripts/blender.sh
+++ Back/src/scripts/blender.sh
@@ -3,5 +3,8 @@
 #/Applications/blender.app/Contents/MacOS/blender
 echo $1
 #for FILE in FINAL/*
-blender --background scene.blend --python blender.py --render-output //render_$1 --engine CYCLES --use-extension 1 --render-frame 1  -- $1
+blender --background scene.blend  --python blender.py --render-output //render_$1 --render-format AVIRAW  -s 1 -e 100 -t 6 -a -- $1
+
 #done
+#-use-extension 1
+
